230239e7889b9e30b2396ce3f4c0412ad2ca2e95
Vasil <58910106+vasetousa@users.noreply.github.com>
2021-10-27 17:26:21 +0200

    adds

--- SOLID/Lab/01_SRP/books.py
+++ SOLID/Lab/01_SRP/books.py
@@ -10,8 +10,73 @@
 #         self.page = page
 #
 
-
 # After
+# class Book:
+#
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
+#
+#     def get_info(self):
+#         return f"This is '{self.title}' by {self.author}"
+#
+#
+# class PageTurner:
+#     page = 0
+#     max_page = 330
+#
+#     def turn_page(self, page):
+#         if page <= 330:
+#             PageTurner.page += page
+#             return f"Turning to page {PageTurner.page} ..."
+#         return f"This book has only {PageTurner.max_page} pages!"
+#
+# class Locator:
+#     def __init__(self, location):
+#         self.location = location
+#
+#     def get_location(self):
+#         return f"This book is located somewhere in {self.location}"
+#
+# class BookRegister:
+#
+#     books = []
+#
+#     def add_book(self, book: Book):
+#         current_book = [b for b in BookRegister.books if b.title == book.title]
+#         if not current_book:
+#             BookRegister.books.append(book)
+#             return f"Book '{book.title}' added to the register!"
+#         return f"This book is already in the register"
+#
+#     def __repr__(self):
+#         lenny = len(BookRegister.books)
+#         nl = "\n"
+#         message = f"We currently have {lenny} book(s) in the register:\n"
+#         for el in BookRegister.books:
+#             message += f">>> '{el.title}', by {el.author}\n"
+#
+#         return message
+#
+#
+#
+#
+# b = Book("The Hole", 'Steven King')
+# b1 = Book("Maugly", "Emilio Salgary")
+# l = Locator("London")
+# p = PageTurner()
+# br = BookRegister()
+# print(br.add_book(b))
+# print(br.add_book(b))
+# print(br.add_book(b1))
+#
+# print(l.get_location())
+# print(p.turn_page(400))
+# print(b.get_info())
+# print(br)
+
+
+# After, another version
 class Book:
     def __init__(self, title, author, location):
         self.title = title
--- SOLID/Lab/03_LSP/ducks.py
+++ SOLID/Lab/03_LSP/ducks.py
@@ -1,24 +1,67 @@
 from abc import abstractmethod, ABC
 
+""" BEFORE """
+
+# class Duck(ABC):
+#     pass
+#
+#
+# class RubberDuck(Duck):
+#     @staticmethod
+#     def quack():
+#         return "Squeek"
+#
+#     @staticmethod
+#     def walk():
+#         """Rubber duck can walk only if you move it"""
+#         raise Exception('I cannot walk by myself')
+#
+#     @staticmethod
+#     def fly():
+#         """Rubber duck can fly only if you throw it"""
+#         raise Exception('I cannot fly by myself')
+#
+#
+# class RobotDuck(Duck):
+#     HEIGHT = 50
+#
+#     def __init__(self):
+#         self.height = 0
+#
+#     @staticmethod
+#     def quack():
+#         return 'Robotic quacking'
+#
+#     @staticmethod
+#     def walk():
+#         return 'Robotic walking'
+#
+#     def fly(self):
+#         """can only fly to specific height but
+#         when it reaches it starts landing automatically"""
+#         if self.height == RobotDuck.HEIGHT:
+#             self.land()
+#         else:
+#             self.height += 1
+#
+#     def land(self):
+#         self.height = 0
+
+
+""" AFTER CORRECTION"""
+
 
 class Duck(ABC):
-    pass
+    @staticmethod
+    @abstractmethod
+    def quack():
+        return "Squeek"
 
 
 class RubberDuck(Duck):
     @staticmethod
     def quack():
         return "Squeek"
-
-    @staticmethod
-    def walk():
-        """Rubber duck can walk only if you move it"""
-        raise Exception('I cannot walk by myself')
-
-    @staticmethod
-    def fly():
-        """Rubber duck can fly only if you throw it"""
-        raise Exception('I cannot fly by myself')
 
 
 class RobotDuck(Duck):
@@ -47,5 +90,21 @@ class RobotDuck(Duck):
         self.height = 0
 
 
+class LiveDuck(RobotDuck, Duck):
+    @staticmethod
+    def quack():
+        return 'quacking'
 
+    @staticmethod
+    def walk():
+        return 'walking'
 
+    def fly(self):
+        return 'flying'
+
+    @staticmethod
+    def eat():
+        return 'eating'
+
+duck = RobotDuck()
+print(duck.fly())
